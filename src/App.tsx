5de503821d0ed409d67c348b0b42d5306bed1e2f
mfkfawas <mfkfawas@gmail.com>
2022-06-23 12:40:16 +0530

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import { useState } from 'react';
+import { Container } from 'react-bootstrap';
+import { Route, Routes } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+import Store from './pages/Store';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <h1>Hai</h1>;
+  return (
+    <Container className='mb-4'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/store' element={<Store />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
