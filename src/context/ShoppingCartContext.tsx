bb08aed16ada134a39e7068580e25bf420a1cb01
mfkfawas <mfkfawas@gmail.com>
2022-06-24 20:45:07 +0530

    getItemQty & increaseItemQty

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -24,7 +24,29 @@ export function useShoppingCart() {
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
+  function getItemQuantity(id: number) {
+    return cartItems.find(item => item.id === id)?.quantity || 0;
+  }
+
+  function increaseCartQuantity(id: number) {
+    setCartItems(currItems => {
+      if (currItems.find(item => item.id === id) === null) {
+        return [...currItems, { id, quantity: 1 }];
+      } else {
+        return currItems.map(item => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
   return (
-    <shoppingCartContext.Provider value={{}}>{children}</shoppingCartContext.Provider>
+    <shoppingCartContext.Provider value={{ getItemQuantity, increaseCartQuantity }}>
+      {children}
+    </shoppingCartContext.Provider>
   );
 }
