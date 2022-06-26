a7f178b5f287d90d817868a087d8688461b56c7f
mfkfawas <mfkfawas@gmail.com>
2022-06-27 01:14:45 +0530

    ShoppingCart total section

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Offcanvas, Stack } from 'react-bootstrap';
 import { useShoppingCart } from '../context/ShoppingCartContext';
+import { formatCurrency } from '../utils/formatCurrency';
 import { CartItem } from './CartItem';
+import storeItems from '../data/items.json';
 
 interface ShoppingCartProps {
   isOpen: boolean;
@@ -20,6 +22,16 @@ const ShoppingCart = ({ isOpen }: ShoppingCartProps) => {
           {cartItems.map(item => (
             <CartItem key={item.id} {...item} />
           ))}
+
+          <div className='ms-auto fw-bold fs-5'>
+            Total{' '}
+            {formatCurrency(
+              cartItems.reduce((total, cartItem) => {
+                const item = storeItems.find(i => i.id === cartItem.id);
+                return total + (item?.price || 0) * cartItem.quantity;
+              }, 0)
+            )}
+          </div>
         </Stack>
       </Offcanvas.Body>
     </Offcanvas>
